4cc271b49bba8414256bf67cb016f6f268bb164a
prince-konwea <kokodecontee123@gmail.com>
2024-07-08 10:46:01 +0100

    feat: add user service to handle users action methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get('roles')
   findAll(): string {
     return 'This action returns all users roles';
-  }
+  }  
+
+  @Get(':id')
+   findOne(@Param('id') id: string): string{
+       return `This action returns a #${id} user`
+   }
+
   @Post('register')
   create(): string{
     return "This action adds a new user"
